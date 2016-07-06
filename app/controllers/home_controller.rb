8978b6826f0b1e621c31dee757abfaeefde0fe28
Eric Nelson <ericn@berkeley.edu>
2016-07-06 03:00:10 +0000

    Very beginnings of Homie: starter INSPINIA template

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+class HomeController < ApplicationController
+  def index
+    @mainTitle = "Welcome to Homie"
+    @mainDesc = "Making living with friends easier."
+  end
+
+  def minor
+  end
+end
