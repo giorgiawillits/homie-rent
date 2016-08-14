9dd8a83560819cac396cb406a3a896e73097e66b
Giorgia Willits <gwillits@berkeley.edu>
2016-08-14 14:43:16 -0700

    reply to say you completed a charge should be working

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -19,20 +19,20 @@ class TwilioController < ApplicationController
     puts :incoming, incoming
     sms_input = params[:Body].downcase
     if sms_input =~ /completed ([0-9]*)/
-      puts $1
       expense = Expense.find_by_id($1)
-      puts expense
       paid_by = expense.paid_by
       user_charged = User.find_by_phone_number(incoming)
       amount = expense.charges.where(:charged_to => user_charged).first.amount
 
       message = "#{user_charged.first_name} says that they have completed your charge of $#{amount} for #{expense.name}. To confirm that they have completed this charge, reply CONFIRM COMPLETED #{expense.id}"
 
+      boot_twilio
       sms = @client.messages.create(
         from: ENV["TWILIO_NUMBER"],
         to: paid_by.phone_number,
         body: message)
     end
+    render xml: {}
   end
 
   private
