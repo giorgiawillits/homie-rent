6fee2a784eaed7308790359953344ff06db70dd9
ericmnelson <ericn@berkeley.edu>
2016-08-16 00:29:21 -0700

    ignore pem file

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,19 +3,19 @@
 class ExpensesController < ApplicationController
   def index
   end
-  
+
   def show
     @show = true
     @expense = Expense.find_by_id(params[:id])
   end
-  
+
   def new
   end
-  
+
   def create
     expense = Expense.new(expense_params)
     expense.paid_by = current_user
-    
+
     charge_to = params[:charges][:users]
     charge_to.each do |user_id, amnt|
       if amnt.to_i > 0
@@ -25,14 +25,14 @@ class ExpensesController < ApplicationController
         charge.save
       end
     end
-    
+
     if expense.save
       redirect_to expenses_path
     else
       redirect_to new_expense_path
     end
   end
-  
+
   def edit
     @expense = Expense.find_by_id(params[:id])
     @charges = {}
@@ -40,25 +40,25 @@ class ExpensesController < ApplicationController
       @charges[charge.charged_to] = charge
     end
   end
-  
+
   def update
     @expense = Expense.find_by_id(params[:id])
     @expense.update_attributes!(expense_params)
     flash[:success] = "The expense has been updated"
     redirect_to expenses_path
   end
-  
+
   def destroy
     @expense = Expense.find_by_id(params[:id])
     @expense.destroy!
     redirect_to expenses_path
   end
-  
+
   private
-  
+
   def expense_params
     params.require(:expense).permit(:name, :amount, :date, :category, :details, :deadline)
   end
-  
-  
+
+
 end
