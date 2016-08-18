09ea73df87ab51f51def978c530bded675051c05
Giorgia Willits <gwillits@berkeley.edu>
2016-08-17 23:14:57 -0700

    for now, adding column owner_id to JOB instead of adding reference owner

--- db/migrate/20160816071901_add_owner_to_delayed_jobs.rb
+++ db/migrate/20160816071901_add_owner_to_delayed_jobs.rb
@@ -1,7 +1,7 @@
 class AddOwnerToDelayedJobs < ActiveRecord::Migration
   def self.up
     add_column :delayed_jobs, :owner_type, :string
-    add_reference :delayed_jobs, :owner, index: false, foreign_key: true
+    add_column :delayed_jobs, :owner_id, :integer
 
     add_index :delayed_jobs, [:owner_type, :owner_id]
   end
