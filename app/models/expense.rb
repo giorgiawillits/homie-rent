3b07c733eac32745d3fcaca30bfb7d81f68de1a6
Giorgia Willits <gwillits@berkeley.edu>
2016-08-18 23:02:25 -0700

    I found the bugheroku run --app homie-rent rake db:seedheroku run --app homie-rent rake db:seedheroku run --app homie-rent rake db:seedheroku run --app homie-rent rake db:seedheroku run --app homie-rent rake db:seedheroku run --app homie-rent rake db:seedheroku run --app homie-rent rake db:seedheroku run --app homie-rent rake db:seedheroku run --app homie-rent rake db:seedheroku run --app homie-rent rake db:seedheroku run --app homie-rent rake db:seedheroku run --app homie-rent rake db:seedheroku run --app homie-rent rake db:seedheroku run --app homie-rent rake db:seedheroku run --app homie-rent rake db:seedheroku run --app homie-rent rake db:seedheroku run --app homie-rent rake db:seedheroku run --app homie-rent rake db:seedheroku run --app homie-rent rake db:seedheroku run --app homie-rent rake db:seedheroku run --app homie-rent rake db:seedheroku run --app homie-rent rake db:seedheroku run --app homie-rent rake db:seedheroku run --app homie-rent rake db:seed

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -9,11 +9,11 @@ class Expense < ActiveRecord::Base
   has_many :jobs, :class_name => "::Delayed::Job", :as => :owner
 
   after_create :reminder
-  # after_save :update_reminders
-  # after_destroy :delete_reminders
+  after_save :update_reminders
+  after_destroy :delete_reminders
 
   # @@REMINDER_TIME = 1.day # days before deadline
-  @@REMINDER_TIME = 1.minute # days before deadline
+  @@REMINDER_TIME = 1.minute # for testing purposes
 
   def date_formatted
     format_date self.date
@@ -63,16 +63,12 @@ class Expense < ActiveRecord::Base
 
   # Notify those charged who havent paid yet X days before the deadline
   def reminder
-    logger.info "called reminder"
-    logger.info "expense: #{self.name}"
     send_reminders self.charges.where(:completed => false)
   end
-
   # handle_asynchronously :reminder, :run_at => Proc.new { |i| i.when_to_run }, :owner => Proc.new { |o| o }
   handle_asynchronously :reminder, :run_at => Proc.new { |i| i.when_to_run }, :owner_type => Proc.new { |o| o.class.name }, :owner_id => Proc.new { |o| o.id }
 
   def send_reminders charges
-    logger.info "called send_reminders"
     @twilio_number = ENV['TWILIO_NUMBER']
     @client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']
     deadline_str = deadline_formatted
@@ -81,15 +77,10 @@ class Expense < ActiveRecord::Base
     expense_name = self.name
 
     charges.each do |charge|
-      logger.info "charge: #{charge.charged_to.first_name}"
       name = charge.charged_to.first_name.capitalize
-      logger.info "name: #{name}"
       amount = charge.amount_formatted_slim
-      logger.info "amount: #{amount}"
       phone_number = charge.charged_to.phone_number
-      logger.info "phone_number: #{phone_number}"
       reminder = "Hi #{name}. Please pay #{paid_by_name} #{amount} for #{expense_name} by #{deadline_str} in order to avoid a late fee of $#{late_fee}. If you have already completed this charge, reply COMPLETED #{charge.id}."
-      logger.info "reminder: #{reminder}"
 
       message = @client.account.messages.create(
         :from => @twilio_number,
@@ -98,15 +89,14 @@ class Expense < ActiveRecord::Base
     end
   end
 
-  # def update_reminders
-  #   if self.jobs.first.run_at != self.when_to_run
-  #     self.jobs.first.update_attributes(:run_at => Proc.new { |i| i.when_to_run })
-  #     # reminder
-  #   end
-  # end
+  def update_reminders
+    if self.jobs.first.run_at != self.when_to_run
+      self.jobs.first.update_attributes(:run_at => Proc.new { |i| i.when_to_run })
+    end
+  end
 
-  # def delete_reminders
-  #   self.jobs.destroy_all
-  # end
+  def delete_reminders
+    self.jobs.destroy_all
+  end
 
 end
